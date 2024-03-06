f865d2c1f83ecd5025539f258dda927c1bd81bad
Verdon Crann <verdon.crann@manchester.ac.uk>
2024-03-06 20:39:24 +0000

    sleep mode change

--- src/main.cpp
+++ src/main.cpp
@@ -156,8 +156,8 @@ void setup_station()
 {
     //-- Connect to an existing network
     ledManager.setLED(ledManager.wifi, ledManager.doubleBlink); // Double blink while searching for station
+    WiFi.setSleepMode(WIFI_NONE_SLEEP);                         // Aparrently it can go to sleep in station mode
     WiFi.mode(WIFI_STA);
-    WiFi.setSleepMode(WIFI_NONE_SLEEP); // Aparrently it can go to sleep in station mode
     WiFi.config(Parameters.getWifiStaIP(), Parameters.getWifiStaGateway(), Parameters.getWifiStaSubnet(), 0U, 0U);
     WiFi.begin(Parameters.getWifiStaSsid(), Parameters.getWifiStaPassword());
 }
